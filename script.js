76872a264401ae450383b9f8671529cb6628a849
Marwan <maro.mohamed7287@gmail.com>
2024-05-29 21:42:56 +0300

    feat: add the greco roman museum

---
+++ script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  }); 
